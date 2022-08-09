f331c238e85ad0435e331650bf594450332fa5d7
diego87 <87.c.die@gmail.com>
2022-08-09 12:46:09 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let nome = "Diego"
+let sobreNome = "Carvalho"
+let anoNasdcimento = 1987
+let idade = 35
+let cidade = "São Bernardo Campo"
+
+console.log(`Eu sou ${nome} ${sobreNome}, tenho ${idade} anos e moro em ${cidade}.`)
