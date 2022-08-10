24a4150b7690afb1113eca7f2e27c0ac5149de9b
diego87 <87.c.die@gmail.com>
2022-08-10 18:30:45 -0300

    corrigido

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let nome = "Diego"
 let sobreNome = "Carvalho"
-let anoNasdcimento = 1987
-let idade = 35
+let anoNascimento = 1987
+let anoAtual = 2022
+let idade = anoAtual - anoNascimento
 let cidade = "São Bernardo Campo"
-
-console.log(`Eu sou ${nome} ${sobreNome}, tenho ${idade} anos e moro em ${cidade}.`)
+console.log(`Eu sou ${nome} ${sobreNome} , tenho ${idade} anos e moro em ${cidade}`)
